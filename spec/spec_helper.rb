0bd04ebcfd0538048541fe9c401e43a4ccbc01f8
Leonardo Couto <leonardowrc@gmail.com>
2013-11-20 16:08:39 -0200

    Adds endpoint directory.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,13 @@ require 'coveralls'
 require 'mail'
 Coveralls.wear!
 
-Dir[File.dirname(__FILE__) + '/../**/*.rb'].each { |file| require file }
+Dir[File.dirname(__FILE__) + '/../dsl/*.rb'].each { |file| require file }
+Dir[File.dirname(__FILE__) + '/../endpoints/**/*.rb'].each { |file| require file }
+Dir[File.dirname(__FILE__) + '/../endpoints/*.rb'].each { |file| require file }
 
 RSpec.configure do |config|
+  config.include Dsl::Smtp
+
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
