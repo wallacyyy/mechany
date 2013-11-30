7b422f0f195e75be28505df44a5444123ba71e02
Leonardo Couto <leonardowrc@gmail.com>
2013-11-29 23:37:22 -0200

    Remove old call.

--- endpoints/soap/request.rb
+++ endpoints/soap/request.rb
@@ -10,9 +10,8 @@ module Endpoint
       attribute :client
 
       def result
-        service = client
         xml = Dsl::Reader.new(file_path: file_path).xml
-        response = service.call(:get_info_by_zip, xml: xml)
+        response = client.call(:get_info_by_zip, xml: xml)
       end
     end
   end
