338d93db73c59dcb771bb8b4c8156bf28a0d2f8a
Leonardo Couto <leonardowrc@gmail.com>
2013-12-01 13:29:27 -0200

    Adds more docs.

--- dsl/reader.rb
+++ dsl/reader.rb
@@ -9,11 +9,15 @@ module Dsl
     # File path that the reader will read.
     attribute :file_path, String
 
+    ##
+    # Retrieves an value on the the yml root.
     def value(key)
       file = YAML.load_file(file_path)
       file[key]
     end
 
+    ##
+    # Read an xml file. Right now this method are reading any file type.
     def xml
       file = File.read(file_path)
       # Add remove line breaks here
