63ceeabd2d1b43198a019821347faa220fcd42d5
Leonardo Couto <leonardowrc@gmail.com>
2014-01-31 15:04:24 -0200

    Adds celluloid on service.

--- services/service.rb
+++ services/service.rb
@@ -1,7 +1,12 @@
 module Mechany
   ##
   # One service can get 1..* endpoints.   
+  # The service itself is an async operation, but the methods through the integration aren't.
   class Service
+    include Celluloid
+
+    attribute :id, Integer
     attribute :endpoints, Array
+
   end
 end
