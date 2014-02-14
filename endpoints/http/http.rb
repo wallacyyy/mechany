b37852c5c2ba102eecee4bbf5035ffb58f6097a5
Leonardo Couto <leonardowrc@gmail.com>
2014-02-14 18:15:29 -0200

    Transform http endpoint in a celluloid actor.

--- endpoints/http/http.rb
+++ endpoints/http/http.rb
@@ -5,6 +5,7 @@ module Endpoint
   class Http
     include Virtus.model
     include HTTParty
+    include Celluloid
 
     # Url that the client will send the request.
     attribute :url, String
@@ -14,5 +15,6 @@ module Endpoint
     def get
       HTTParty.get(url)
     end
+
   end
 end
