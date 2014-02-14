ed8d937ccf24aa574b625010bc58ed4f6f368117
Leonardo Couto <leonardowrc@gmail.com>
2014-02-14 18:16:07 -0200

    Adds sample method and service working workflow explanation.

--- dsl/service.rb
+++ dsl/service.rb
@@ -2,21 +2,42 @@ module Dsl
   require_relative '../endpoints/mailing/delivery'
   require_relative '../endpoints/soap/request'
   require_relative '../endpoints/http/http'
+  require_relative '../dsl/reader'
 
   ##
   # Reads dsl files and delegates the params to services. 
   class Service 
+    include Virtus.model
+    include Celluloid
 
-    ENDPOINTS = {
-      :mail => Endpoint::Mailing::Delivery,
-      :soap => Endpoint::Soap::Request,
-      :http => Endpoint::Http
-    }
+    attribute :reader, Dsl::Reader
+
+    def endpoints
+      { :mail => Endpoint::Mailing::Delivery,
+        :soap => Endpoint::Soap::Request,
+        :http => Endpoint::Http }
+    end
 
     ##
-    # Call an endpoint.  
-    def self.call(endpoint)
-      ENDPOINTS[endpoint.to_sym]
+    # Call an endpoint  
+    # An service calls an execution pipeline of endpoints, transfering
+    # the response of one to the default receiver field of the next endpoint.
+    # Each node is locked on exclusive mode until finish.
+    # An group of services is called by an client, the service results is received
+    # with an pipeline of async celluloid futures.
+    # The services are async between them, but sync by itself.
+    def run
+      nodes = reader.nodes
+      response = nil
+      nodes.each do |node|
+        exclusive do
+          clazz = endpoints[node.to_sym]
+          endpoint = clazz.new(node)
+          result = endpoint.call(node, parameters, response)
+          response = result
+          endpoint.terminate
+        end
+      end
     end
 
   end
