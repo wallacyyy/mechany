f57656cb0e615cfeef816a82acbd865445a52dfe
Leonardo Couto <leonardowrc@gmail.com>
2014-03-14 13:28:17 -0300

    Fix request spec for new soap request structure.

--- spec/endpoints/request_spec.rb
+++ spec/endpoints/request_spec.rb
@@ -4,10 +4,14 @@ describe Endpoint::Soap::Request do
 
   let(:client) { double('client') }
 
-  it 'sends an request through savon' do
-    request = Endpoint::Soap::Request.new(client: client)
-    expect(File).to receive(:read).and_return('random request')
-    expect(client).to receive(:call)
-    request.result(:get_info_by_zip)
+  it 'sends an request through client' do
+    wsdl = '#butatwhatcost'
+    payload ='random request' 
+    request = Endpoint::Soap::Request.new(wsdl: wsdl)
+    expect(File).to receive(:read).and_return(payload)
+    expect(Endpoint::Soap::Client).to receive(:new).with({ wsdl: wsdl }).and_return(client)
+    expect(client).to receive(:instance).and_return(instance = double('instance'))
+    expect(instance).to receive(:call).with(:operation, { xml: payload })
+    request.result(:operation)
   end
 end
