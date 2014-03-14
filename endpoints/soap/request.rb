94e86b57bcc3cf7f1af167c3be60dd46ae053f48
Leonardo Couto <leonardowrc@gmail.com>
2014-03-14 13:13:58 -0300

    Remove client dependency from soap.

--- endpoints/soap/request.rb
+++ endpoints/soap/request.rb
@@ -2,6 +2,7 @@ module Endpoint
   module Soap
     require 'savon'
     require_relative '../../dsl/reader'
+    require_relative 'client'
     ##
     # Handles the requests to SOAP web services. 
     class Request
@@ -10,13 +11,14 @@ module Endpoint
       # The path to file that contains a request.
       attribute :file_path, String
       # Client that will be called to send the request.
-      attribute :client
+      attribute :wsdl
 
       ##
       # Sends the request to an soap web service through the client.
       # Expects the operation name as symbol
       def result(operation)
         xml = Dsl::Reader.new(file_path: file_path).xml
+        client = Endpoint::Soap::Client.new(wsdl: wsdl).instance
         client.call(operation, xml: xml)
       end
     end
--- spec/endpoints/integration/soap_spec.rb
+++ spec/endpoints/integration/soap_spec.rb
@@ -4,12 +4,10 @@ describe Endpoint::Soap::Request do
 
   let(:path) { Dir.pwd + '/spec/fixtures/requests/us_zip_request.xml' }
   let(:wsdl) { 'http://www.webservicex.net/uszip.asmx?WSDL' }
-  let(:client) { Endpoint::Soap::Client.new(wsdl: wsdl) }
 
   it 'sends an request and retrieves the result' do
-    savon_client = client.with_savon
     VCR.use_cassette('us_zip_response') do
-      request = Endpoint::Soap::Request.new(file_path: path, client: savon_client)
+      request = Endpoint::Soap::Request.new(file_path: path, wsdl: wsdl )
       expect(request.result(:get_info_by_zip).doc.search("//CITY").text).to eq("Beverly Hills")
     end
   end
